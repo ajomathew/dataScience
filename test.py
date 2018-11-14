5aa4ebb6d460cf36d4e11ae05b89f290208f51cc
Ajo Mathew <ajomathiu@gmail.com>
2018-11-15 00:03:24 +0530

    Checking data types

--- test.py
+++ test.py
@@ -5,3 +5,5 @@ df.head(2)
 headers = ["symboling","normalized-losses","make","fuel-type","aspiration","num-of-doors","body-style","drive-wheels","engine-location","wheel-base","length","width","height","curb-weight","engine-type","num-of-cylinders","engine-size","fuel-system","bore","stroke","compression-ratio","horsepower","peak-rpm","city-mpg","highway-mpg","price"]
 df.columns = headers
 df.to_csv('test.csv')
+#Checking data tupes of data sets
+df.dtypes
